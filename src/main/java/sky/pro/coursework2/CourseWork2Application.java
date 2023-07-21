664e2918229184d3a280f2ab3ae7069867ef9e8a
zhannatkachenko <zhanna.kindgirl@gmail.com>
2023-07-21 16:39:11 +0300

    initial

---
+++ src/main/java/sky/pro/coursework2/CourseWork2Application.java
@@ -0,0 +1,13 @@
+package sky.pro.coursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/coursework2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
